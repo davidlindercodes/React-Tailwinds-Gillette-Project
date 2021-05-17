11458d795dcc15d64d70fd0eac1ba70a83271e26
David Linder <56964108+davidlindercodes@users.noreply.github.com>
2021-05-17 07:42:49 +0100

    burger menu added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,19 @@ const Navbar = () => {
 
                 <section className="pt-3 block sm:hidden">
                 <Link to="/" className='ml-4 mr-4 tracking-tight'>
-                Burger Here
+            <svg 
+                    class="w-9 h-9 fixed top-5 right-5" 
+                    fill="none" 
+                    stroke="currentColor" 
+                    viewBox="0 0 24 24" 
+                    xmlns="http://www.w3.org/2000/svg">
+                        
+                <path 
+                    stroke-linecap="round" 
+                    stroke-linejoin="round" 
+                    stroke-width="2" d="M4 6h16M4 12h16M4 18h16">
+                </path>
+            </svg>
                 </Link>
                 
             
